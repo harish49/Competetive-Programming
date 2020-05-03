510c42cf75e5ce5a336ba6804077f084523b269c
Harish Vemula <43724708+harish49@users.noreply.github.com>
2020-05-03 16:17:51 +0530

    Update Ransom Note.cpp

--- MayLeetCode-Challenge/Ransom Note.cpp
+++ MayLeetCode-Challenge/Ransom Note.cpp
@@ -1,1 +1,19 @@
 
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        map<char,int>a,b;
+        for(int i = 0 ; i < ransomNote.size() ; i++){
+            a[ransomNote[i]]++;
+        }
+        for(int i = 0 ; i < magazine.size() ; i++ ){
+            b[magazine[i]]++;
+        }
+        for(auto p : a){
+            if(b[p.first]<p.second){
+                return false;
+            }
+        }
+        return true;
+    }
+};

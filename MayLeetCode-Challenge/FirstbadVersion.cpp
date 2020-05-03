cb6a4927ea567c3afed68e1943024aa60bf6ef0e
Harish Vemula <43724708+harish49@users.noreply.github.com>
2020-05-03 16:16:07 +0530

    Update FirstbadVersion.cpp

--- MayLeetCode-Challenge/FirstbadVersion.cpp
+++ MayLeetCode-Challenge/FirstbadVersion.cpp
@@ -1,1 +1,26 @@
 
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int solve(int n){
+        int start = 1;
+        int end = n;
+        int ans = -1;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            if(isBadVersion(mid)==true){
+                ans = mid;
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        return ans;
+    }
+    int firstBadVersion(int n) {
+        return solve(n);
+    }
+};

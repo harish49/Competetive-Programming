ad4a4f1c75258e28f29651d8b4bda6e5e6690c43
Harish Vemula <43724708+harish49@users.noreply.github.com>
2020-05-03 16:17:00 +0530

    Update Jewels and Stones.cpp

--- MayLeetCode-Challenge/Jewels and Stones.cpp
+++ MayLeetCode-Challenge/Jewels and Stones.cpp
@@ -1,1 +1,15 @@
 
+class Solution {
+public:
+    int numJewelsInStones(string J, string S) {
+      map<char,int>mp;
+      for(int i = 0 ; i < S.size() ; i++){
+          mp[S[i]]++;
+      }
+        int count = 0;
+        for(int i = 0 ; i < J.size() ; i++){
+            count+=mp[J[i]];
+        }
+        return count;
+    }
+};

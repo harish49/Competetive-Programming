509accede59060d80941df476744d250d73e21e6
harish_vemula <>
2021-07-22 15:51:29 +0530

    Updated cpp template

---
+++ cpp-template.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#define mod 1000000007
+#define int long long
+#define pii pair<int,int>
+#define pb(x) push_back(x)
+#define lbound lower_bound
+#define ubound upper_bound
+#define gcd(a,b) __gcd(a,b)
+#define all(v) v.begin(),v.end()
+#define bits(x) __builtin_popcountll(x)
+#define heap_min priority_queue<int,vector<int>,greater<int>>
+#define heap_max priority_queue<int>
+#define precision(x) cout << setprecision(x) << fixed;
+#define printv(v) for(auto p : v) cout << p << " ";
+#define printa(a,n) for(int i = 0 ; i < n ; i++) cout << a[i] << " ";
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+using namespace std;
+
+
+void solve()
+{
+
+}
+
+int32_t main()
+{
+   fast;
+   #ifndef ONLINE_JUDGE
+   freopen("goodluckin.txt","r",stdin);
+   freopen("goodluckout.txt","w",stdout);
+   #endif
+   clock_t beg = clock();
+   solve();
+   clock_t end = clock();
+   fprintf(stderr, "Time taken : %.3f sec\n", double(end - beg) / CLOCKS_PER_SEC , 184);
+   return 0;
+
+}
